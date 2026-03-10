709df4156be978b942655dcbd6b570f1ad41c26d
chinedu winner chibundo <chineduwinnerchibundo@gmail.com>
2026-03-10 21:48:36 +0100

    add server folder

--- my-node-project/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/loginDB")
-.then(() => {
-    console.log("Database connected successfully");
-})
-.catch((err) => {
-    console.log(err);
-});
